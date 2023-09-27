db277a378bbcbe1ffaf51ca3db86f35957f50972
Milan <4k.milan15@gmail.com>
2023-09-27 11:34:51 +0500

    add_correct_incorrect_markers

--- script/main.js
+++ script/main.js
@@ -14,18 +14,10 @@ let middleHardQuestions = document.querySelector('.middle_hard_questions');
 let HardQuestions = document.querySelector('.hard_questions');
 let PopUpWindow = document.querySelector('#popup_main_1');
 let closePopUpButton = document.querySelector('#close_popup_button_1');
-
+let correctMarkerPlace = document.querySelector('#correct_marker_1');
 
 
 // Очень легкие вопросы массив
-let QuestionsList = [
-    {
-        text: '2 + 2 = ?',
-        right: 0,
-        answers: ['1', '2', '3', '4',],
-        point: 100,
-    },
-]; 
 
 
 // Вытаскиваем ID кнопок из HTML
@@ -33,14 +25,24 @@ let veryEasyQuestion1 = document.querySelector('#very_easy_question_1');
 let veryEasyQuestion2 = document.querySelector('#very_easy_question_2');
 let veryEasyQuestion3 = document.querySelector('#very_easy_question_3');
 let parseQuestionPlace = document.querySelector('#text_question_1');
+let pointsOfAttemptPlace= document.querySelector('#points_of_attempt_1');
+
+// Массив с вопросами
+let questions = [
+    {
+        text: 'На какой свет можно ехать?',
+        right: 0,
+        answers: ['зеленый', 'красный', 'желтый', 'синий',],
+    },
+];
 
 
 
-for(let QuestionsListEasy of QuestionsList){
+for(let question of questions){
     veryEasyQuestion1.addEventListener('click', function(){
         PopUpWindow.classList.remove('closed');
         PopUpWindow.classList.add('open');
-        
+        console.log(question)
     });
 
     let test = document.querySelector('#question_number_1');
@@ -55,17 +57,10 @@ for(let QuestionsListEasy of QuestionsList){
 
     Dalee.classList = ('disabled_button');
 
-    let questions = [
-        {
-            text: 'На какой свет можно ехать?',
-            right: 0,
-            answers: ['1', '2', '3', '4',],
-        },
-    ];
 
-    let answersA1 = ['зеленый', 'красный', 'желтый', 'синий',];
 
     let i = 1;
+// Рендер вопросов из списка
     for (let question of questions){
         let div = document.createElement('div');
         test.appendChild(div);
@@ -90,7 +85,7 @@ for(let QuestionsListEasy of QuestionsList){
             input.dataset.answerNum = j++;
             divInp.appendChild(input);
             let answ = document.createElement('p');
-            answ.innerHTML = answersA1[a++];
+            answ.innerHTML = answer;
             divInp.appendChild(answ);
         };
     };
@@ -107,31 +102,39 @@ for(let QuestionsListEasy of QuestionsList){
                         let disButton = document.querySelector('#check_button_1');
                         if (input.dataset.answerNum == form.dataset.right){
                             arrPoints.push(100)
-                            
+                            let imgCorrect = document.createElement('img');
+                            imgCorrect.src = './content/correct.svg';
+                            correctMarkerPlace.appendChild(imgCorrect);
+                            pointsOfAttemptPlace.innerHTML = pointsOfAttempt - 1;
+
+
                             disButton.classList.remove('disabled_button');
                             Povtor.classList.add('disabled_button');
                             Dalee.classList.add('disabled_button');
 
                             form.children[0].classList.add('correct');
-                            form.children[1].classList.add('incorrect2');
-                            form.children[2].classList.add('incorrect2');
-                            form.children[3].classList.add('incorrect2');
-                            form.children[0].children[0].disabled = true;
-                            form.children[1].children[0].disabled = true;
-                            form.children[2].children[0].disabled = true;
-                            form.children[3].children[0].disabled = true;
+                            for(let i = 1; i < 4; i++){
+                                form.children[i].classList.add('incorrect2');
+                            }
+                            for(let i = 0; i < 4; i++){
+                                form.children[i].children[0].disabled = true;
+                            }
                         }else{
-                            
+                            let imgCorrect = document.createElement('img');
+                            imgCorrect.src = './content/incorrect.svg';
+                            correctMarkerPlace.appendChild(imgCorrect);
+                            pointsOfAttemptPlace.innerHTML = pointsOfAttempt - 1;
+
                             disButton.classList.remove('disabled_button');
                             Povtor.classList.add('disabled_button');
                             Dalee.classList.add('disabled_button');
                             let wrAns = input.dataset.answerNum
-                            form.children[wrAns].classList.add('incorrect');
                             
-                            form.children[0].children[0].disabled = true;
-                            form.children[1].children[0].disabled = true;
-                            form.children[2].children[0].disabled = true;
-                            form.children[3].children[0].disabled = true;
+                            form.children[wrAns].classList.add('incorrect');
+
+                            for(let i = 0; i < 4; i++){
+                                form.children[i].children[0].disabled = true;
+                            }
                         };
                     break;
                 };
